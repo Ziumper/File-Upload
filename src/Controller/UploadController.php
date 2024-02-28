8e53bbfe4ee894548db9c7a9e69bdd72874d19aa
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2024-02-28 10:31:17 +0100

    add upload controller

---
+++ src/Controller/UploadController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class UploadController  extends AbstractController {
+
+    #[Route('/upload',name:"upload")]    
+    public function uploadAction(Request $request) :  JsonResponse {
+        return new JsonResponse([
+            "success"=> true
+        ]);
+    }
+
+    // #[Route('/upload/list', name: "upload_list")]
+    // public function uploadListAction(Request $request) {
+
+    // }
+
+}
