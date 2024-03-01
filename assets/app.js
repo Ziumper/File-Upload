1fa59d068293b30d5b5e4233752a6ef63466ccdc
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2024-03-01 19:41:19 +0100

    frontend validation and adjustment

--- assets/app.js
+++ assets/app.js
@@ -9,6 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 
 $(function() {
+
     const me = this;
     me.elements = {
         form : "#uploadForm",
@@ -17,8 +18,13 @@ $(function() {
         info: "#info",
         name: "#name",
         surname: "#surname",
-        file: "#formFile"
+        file: "#formFile",
+        notValidFileElement: "#fileInvalidMessage"
     };
+
+    me.allowedExtensions = [
+        "jpg","gif","png","jpeg"
+    ]
 
     me.hideElement = function(selector) {
         $(selector).addClass("hide");
@@ -28,15 +34,61 @@ $(function() {
         $(selector).removeClass("hide");
     };
 
-    $(me.elements.form).submit(function(e) {
+    me.getFileExtenison = function(fileName) {
+        var extension = fileName.split(".");
+        if( extension.length === 1 || ( extension[0] === "" && extension.length === 2 ) ) {
+            return "";
+        }
+        return extension.pop(); 
+    }
+
+    me.validateFileInput = function() {
+        //custom file validation
+        const fileInput = $(me.elements.file)[0];
+        if(fileInput.files.length) {
+            const file = fileInput.files[0];
+            if(file.size > 1024*1024) {
+                $(me.elements.notValidFileElement).html("The file is too big!");
+                fileInput.setCustomValidity("The file is too big!");
+                return false;
+            }
+
+            var extension = me.getFileExtenison(file.name);
+            if(!me.allowedExtensions.includes(extension)) {
+                fileInput.setCustomValidity("Not valid extension");
+                $(me.elements.notValidFileElement).html("Not valid extension");
+                return false;
+            }
+        }
+
+        fileInput.setCustomValidity("");
+        return true;
+    }
+
+    $(me.elements.form).submit(function(event) {
         me.hideElement(me.elements.success);
         me.hideElement(me.elements.error);
 
-        //prevent Default functionality
-        e.preventDefault();
+        event.preventDefault();
+
+        var form = document.querySelector('.need-validation');
+
+        if (!form.checkValidity()) {
+            event.stopPropagation()
+            me.validateFileInput();
+            form.classList.add('was-validated')
+            return;
+        }
+
+        if(!me.validateFileInput())
+        { 
+            event.stopPropagation();
+            form.classList.add('was-validated')
+            return;
+        }
         
         //get the action-url of the form
-        var actionurl = e.currentTarget.action;
+        var actionurl = event.currentTarget.action;
         me.showElement(me.elements.info);
 
         var dataForm = new FormData();
